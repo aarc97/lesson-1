644cb52258722011609b3e776e0cd1c04a02eebb
aarc97 <aarc97@gmail.com>
2020-01-17 15:46:48 -0400

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding component with new redux flow for directory/shop

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem
+} from "../../redux/cart/cart.actions";
+
+import "./checkout-item.styles.scss";
+
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+
+  const clearItemHandler = () => {
+    clearItem(cartItem);
+  };
+
+  const addItemHandler = () => {
+    addItem(cartItem);
+  };
+  const removeItemHandler = () => {
+    removeItem(cartItem);
+  };
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={removeItemHandler}>
+          &#10094;
+        </div>
+        <div className="value">{quantity}</div>
+        <div className="arrow" onClick={addItemHandler}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={clearItemHandler}>
+        &#10005;
+      </div>
+    </div>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
